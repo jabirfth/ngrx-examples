f982f3305304dbef0c174333a1987c18074d5e4d
jabirfth <elftouhjabir@gmail.com>
2018-03-18 18:43:27 +0000

    first commit, ng5 material project

---
+++ src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppComponent } from './app.component';
+import { MatModule } from './mat.module';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/mat.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from '@angular/core';
+import { MatToolbarModule, MatSidenavModule } from '@angular/material';
+
+@NgModule({
+  imports: [MatToolbarModule],
+  exports: [MatToolbarModule]
+})
+export class MatModule { }
